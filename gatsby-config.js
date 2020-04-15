e9db28041e1807f7cd049a3950cbfbd86ebb2840
Rowan-James Tran <rowanjames.tran@gmail.com>
2020-04-14 22:13:53 -0700

    Update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,13 +37,13 @@ module.exports = {
         {
             resolve: `gatsby-plugin-manifest`,
             options: {
-                name: `gatsby-starter-default`,
-                short_name: `starter`,
+                name: `2020 A.S. Elections`,
+                short_name: `2020as`,
                 start_url: `/`,
-                background_color: `#663399`,
-                theme_color: `#663399`,
+                background_color: `#003660`,
+                theme_color: `#003660`,
                 display: `minimal-ui`,
-                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+                icon: `src/images/favicon.png`, // This path is relative to the root of the site.
             },
         },
         // this (optional) plugin enables Progressive Web App + Offline functionality
